998a3f70364067f7855dbedce99e74b8a6f78206
ulfur <ulfur@controlant.com>
2024-02-20 18:59:18 +0000

    A simple working UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import MyButton from "./components/Button";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [count, setCount] = useState<number>(0);
+
+    function handleClick() {
+        setCount(count + 1);
+    }
+
+    return (
+        <div>
+            <div>Count: {count}</div>
+            <MyButton onClick={handleClick}/>
+            <MyButton onClick={handleClick}/>
+        </div>
+    );
 }
 
 export default App;
